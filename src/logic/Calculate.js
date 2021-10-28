68e2fe6b71fae7c2985fc215ad5964da0b5ce224
victor-chukwu <okenwa1993@gmail.com>
2021-10-28 20:09:36 +0100

    Checked linters

--- src/logic/Calculate.js
+++ src/logic/Calculate.js
@@ -82,7 +82,6 @@ export default function calculate(obj, buttonName) {
     return { ...obj, operation: buttonName };
   }
 
-  // User pressed an operation button and there is an existing operation
   if (obj.operation) {
     if (obj.total && !obj.next) {
       return { ...obj, operation: buttonName };
@@ -95,14 +94,10 @@ export default function calculate(obj, buttonName) {
     };
   }
 
-  // no operation yet, but the user typed one
-
-  // The user hasn't typed a number yet, just save the operation
   if (!obj.next) {
     return { operation: buttonName };
   }
 
-  // save the operation and shift 'next' into 'total'
   return {
     total: obj.next,
     next: null,
