2e0ae69d586e91e35bfc6ad543fb4d04a17e43d0
victor-chukwu <okenwa1993@gmail.com>
2021-10-28 16:24:49 +0100

    Added operation to calculator

--- src/component/Calculator.js
+++ src/component/Calculator.js
@@ -1,33 +1,56 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
+import calculate from '../logic/Calculate';
 
 class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: 0,
+      next: null,
+      operation: null,
+    };
+  }
+
+  buttonClicked = (e) => {
+    const buttonName = e.target.textContent;
+    const newObj = calculate({ ...this.state }, buttonName);
+    this.setState(newObj);
+  };
+
   render() {
+    const { total, next, operation } = this.state;
+    let result = '';
+    if (total) {
+      result = `${total} ${operation || ''} ${next || ''}`;
+    } else if (next) {
+      result = next;
+    }
     return (
       <>
         <div className="calculator-body">
           <div className="output">
-            <div className="previous-input" />
-            <div className="current-input" />
+            <div className="previous-input">{result}</div>
           </div>
-          <button type="button" className="double-space">AC</button>
-          <button type="button">DEL</button>
-          <button type="button" className="special">/</button>
-          <button type="button">1</button>
-          <button type="button">2</button>
-          <button type="button">3</button>
-          <button type="button" className="special">*</button>
-          <button type="button">4</button>
-          <button type="button">5</button>
-          <button type="button">6</button>
-          <button type="button" className="special">+</button>
-          <button type="button">7</button>
-          <button type="button">8</button>
-          <button type="button">9</button>
-          <button type="button" className="special">-</button>
-          <button type="button" className="double-space">0</button>
-          <button type="button">.</button>
-          <button type="button" className="special">=</button>
+          <button type="button" onClick={(e) => this.buttonClicked(e)}>AC</button>
+          <button type="button" onClick={(e) => this.buttonClicked(e)}>+/-</button>
+          <button type="button" onClick={(e) => this.buttonClicked(e)}>%</button>
+          <button type="button" className="special" onClick={(e) => this.buttonClicked(e)}>/</button>
+          <button type="button" onClick={(e) => this.buttonClicked(e)}>1</button>
+          <button type="button" onClick={(e) => this.buttonClicked(e)}>2</button>
+          <button type="button" onClick={(e) => this.buttonClicked(e)}>3</button>
+          <button type="button" className="special" onClick={(e) => this.buttonClicked(e)}>x</button>
+          <button type="button" onClick={(e) => this.buttonClicked(e)}>4</button>
+          <button type="button" onClick={(e) => this.buttonClicked(e)}>5</button>
+          <button type="button" onClick={(e) => this.buttonClicked(e)}>6</button>
+          <button type="button" className="special" onClick={(e) => this.buttonClicked(e)}>+</button>
+          <button type="button" onClick={(e) => this.buttonClicked(e)}>7</button>
+          <button type="button" onClick={(e) => this.buttonClicked(e)}>8</button>
+          <button type="button" onClick={(e) => this.buttonClicked(e)}>9</button>
+          <button type="button" className="special" onClick={(e) => this.buttonClicked(e)}>-</button>
+          <button type="button" className="double-space" onClick={(e) => this.buttonClicked(e)}>0</button>
+          <button type="button" onClick={(e) => this.buttonClicked(e)}>.</button>
+          <button type="button" className="special" onClick={(e) => this.buttonClicked(e)}>=</button>
 
         </div>
       </>
