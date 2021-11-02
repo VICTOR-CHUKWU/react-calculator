12c1ee9c167d1f4df829df249e2d665c57b975f8
victor-chukwu <okenwa1993@gmail.com>
2021-11-02 21:44:49 +0100

    First commit

---
+++ src/component/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Header.module.css';
+
+const Header = () => (
+  <div className={styles.header}>
+    <h2>Math Magician</h2>
+    <div>
+      <Link to="/"><span>Home</span></Link>
+      <Link to="/calculator"><span>calculator</span></Link>
+      <Link to="/quote"><span>Quote</span></Link>
+    </div>
+  </div>
+);
+
+export default Header;
---
+++ src/component/home/Home.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styles from './Home.module.css';
+
+const Home = () => (
+  <div className={styles.home}>
+    <h2>
+      Welcome to our page
+    </h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Velit, quis explicabo. Quam amet quo blanditiis dolor?
+      Quod voluptatum eum velit expedita repellat laboriosam sunt blanditiis optio,
+      nihil culpa neque quos laborum corporis dolorum, tempore ipsam.
+      Tempore eius nulla explicabo harum!
+
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Velit, quis explicabo. Quam amet quo blanditiis dolor?
+      Quod voluptatum eum velit expedita repellat laboriosam sunt blanditiis optio,
+      nihil culpa neque quos laborum corporis dolorum, tempore ipsam.
+      Tempore eius nulla explicabo harum!
+
+    </p>
+  </div>
+);
+
+export default Home;
